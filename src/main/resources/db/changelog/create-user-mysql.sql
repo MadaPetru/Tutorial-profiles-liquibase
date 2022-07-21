09eb4dee4ec280e61d30e2c5f62950a0ea69da88
MadaPetru <mada.petru@digitalexplorer.ro>
2022-07-21 10:55:57 +0300

    add project to git

---
+++ src/main/resources/db/changelog/create-user-mysql.sql
@@ -0,0 +1,7 @@
+CREATE TABLE Users1 (
+    id int,
+    name varchar(255),
+    password varchar(255),
+    adress varchar(255),
+    job varchar(255)
+);
---
+++ src/main/resources/db/changelog/insert-record-user.sql
@@ -0,0 +1,2 @@
+INSERT INTO Users1 (id, name, password, adress, job)
+VALUES (1, 'adi adi', 'password', 'calea-mare', 'fotbalist');
